961808c184d11d5acfef5dffe824a16e4a8c41f0
Matti Jokitulppo <matti.thebest@hotmail.com>
2014-12-04 21:45:48 +0200

    Logging in and registering functionality

---
+++ src/java/DatabaseManager.java
@@ -0,0 +1,34 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matti
+ */
+public class DatabaseManager {
+    public static String DBurl = "jdbc:mysql://localhost/restinapi";
+    public static String DBuser = "root";
+    public static String DBpassword = "";
+    
+    public static Connection getConnection()
+    {
+        try {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(DBurl, DBuser, DBpassword);
+        return conn;
+            
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+}
---
+++ src/java/LoginBean.java
@@ -0,0 +1,129 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.faces.bean.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matti
+ * 
+ */
+@SessionScoped
+@ManagedBean(name = "loginBean")
+public class LoginBean implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String username;   
+    private String password;
+    private boolean isLoggedIn = false;
+    
+    public LoginBean()
+    {
+        
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIsLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setIsLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+    
+    public void register() {
+        try {
+        Connection conn = DatabaseManager.getConnection();
+        PreparedStatement ps = null;
+                
+        ps = conn.prepareStatement("INSERT INTO  user (name, password) VALUES(?, ?)");
+        String hashedPass = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println("hash:" + hashedPass);
+        ps.setString(1, username);
+        ps.setString(2, hashedPass);
+        
+        int affected = ps.executeUpdate();
+        
+        if (affected > 0)
+        {
+                        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registering succeeded!", ""));
+                        isLoggedIn = true;
+        }
+        }
+        catch (Exception ex) {
+            System.out.println("register error: " + ex.getMessage());
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Registering failed!", "Try again!"));
+        }
+
+    }
+
+        
+    public String login() {
+    try {
+        Connection conn = DatabaseManager.getConnection();
+        PreparedStatement ps = null;
+                
+        ps = conn.prepareStatement("select name, password FROM user where name = ? ");
+        
+        ps.setString(1, username);
+        
+        ResultSet rs = ps.executeQuery();
+        
+        if (rs.next())
+        {            
+            String hashedPass = rs.getString("password");
+            
+            System.out.println("pass: " + password);
+            System.out.println("hashedpass: " + hashedPass);
+            if (BCrypt.checkpw(password, hashedPass))
+            {
+                isLoggedIn = true;
+                return "main";
+            }
+        }
+      
+       isLoggedIn = false;
+       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Username or password invalid", "Try again!"));
+       return "";
+        
+    } catch (Exception ex) {
+        System.out.println("Error loggin in: " + ex.getMessage());
+        return "";
+    }
+    
+    }
+   
+    public void logout() {
+     HttpSession currentSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+     currentSession.invalidate();
+    }
+
+}
