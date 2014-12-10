0396c7b3f7e7fe2d6b18585502665a11220afaec
Jonah Ahvonen <jonah.ahvonen@gmail.com>
2014-12-10 12:12:46 +0200

    ...
    H̰̙͓̝̻͎̦̬̥͇͈̐͗̔̐̑̑ͧ̈́͛̔̑ͧ̏̃̾̀ͫ͘͝͝È̢͙̼͙͔̲̩͔̖̜̩̺͉͍̹͆̌̓̿͒̔̎̀͞L̸̴̢ͦ̂̔̓͂̂͗́̿͒̐ͪ́҉̝͔͇͈̜̯͓͇̞̣̫P͓̱̫̹͚̼̲̤̘̻̬̼͖͕̫̜͛̀̈͂̇ͬͧ̂̕͟
    ̸͑̀̔̔̇̐̅͛ͪ̅͌̾ͨ̎̔͛̍͊̀͏̴̶̨̠̬̝̹̲̫̫̼̖̣̮̺̲̱̮̱͇ͅͅM̃ͭ͊́̈́̒ͯ̔̅͋ͩ̐ͫ̌̑ͦ͏͏̖̲̺̫͎̼̤̳͓̱͇͖̠͙̘̪̳́̕͡Eͩ́͆̈́ͯ͌̾ͨ̓̀҉̵̝͇̙̙͍̫͎̹͚̣̻͚̩͙͜ͅͅ
    
    H̰̙͓̝̻͎̦̬̥͇͈̐͗̔̐̑̑ͧ̈́͛̔̑ͧ̏̃̾̀ͫ͘͝͝È̢͙̼͙͔̲̩͔̖̜̩̺͉͍̹͆̌̓̿͒̔̎̀͞L̸̴̢ͦ̂̔̓͂̂͗́̿͒̐ͪ́҉̝͔͇͈̜̯͓͇̞̣̫P͓̱̫̹͚̼̲̤̘̻̬̼͖͕̫̜͛̀̈͂̇ͬͧ̂̕͟
    ̸͑̀̔̔̇̐̅͛ͪ̅͌̾ͨ̎̔͛̍͊̀͏̴̶̨̠̬̝̹̲̫̫̼̖̣̮̺̲̱̮̱͇ͅͅM̃ͭ͊́̈́̒ͯ̔̅͋ͩ̐ͫ̌̑ͦ͏͏̖̲̺̫͎̼̤̳͓̱͇͖̠͙̘̪̳́̕͡Eͩ́͆̈́ͯ͌̾ͨ̓̀҉̵̝͇̙̙͍̫͎̹͚̣̻͚̩͙͜ͅͅ

--- web/resources/js/fuzz_scripts.js
+++ web/resources/js/fuzz_scripts.js
@@ -1,17 +1,10 @@
 $(document).ready(function() 
 {
-    var jBubby;
-    try
-    {
-    jBubby = $.parseJSON($('#jsonned').val().toString());
-    }
-    catch(eeee)
-    {
-        console.log(eeee);
-    }
+    var jBubby = $.parseJSON($('#jsonned').val().toString());
 
     for(var i = 0; i < jBubby.fields.length; i++)
     {
         console.log(jBubby.fields[i].name + ", " + jBubby.fields[i].value + ", " + jBubby.fields[i].islocked);
     }
+    
 });
